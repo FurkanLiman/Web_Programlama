5afa79b09aa3e7747408f40dc1a696f78f69a088
liman <liman@FURKAN-PC>
2022-12-26 18:16:40 +0300

    Proje dosyası ekle.

---
+++ g201210007_WebOdev/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using g201210007_WebOdev.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace g201210007_WebOdev.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ g201210007_WebOdev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace g201210007_WebOdev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
