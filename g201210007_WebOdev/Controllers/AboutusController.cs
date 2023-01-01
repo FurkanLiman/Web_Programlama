2eb654d11cb0d6ba3ffde0f859f1de36310cc098
Furkan Liman <limanfurkan@outlook.com>
2023-01-01 14:53:46 +0300

    .

---
+++ g201210007_WebOdev/Controllers/AboutusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace g201210007_WebOdev.Controllers
+{
+    public class AboutusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
